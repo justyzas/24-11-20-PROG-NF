cd864fbf7c8cd6c281d0591338fbf15cb313e695
JustelioCode <krutikovasjustinas@gmail.com>
2025-01-28 11:46:40 +0200

    2025-01-28 paskaitos kodas

--- Javascript/2025-01-27 callbacks + events/callbackai.js
+++ Javascript/2025-01-27 callbacks + events/callbackai.js
@@ -9,7 +9,6 @@ const button = document.querySelector("img");
 // 2. pridėsime event listenerį
 // Pridėti įvykio stebėjimą
 
-button.addEventListener("mouseup", function(){
-   console.log("Neužstok saulės")
+button.addEventListener("mouseup", function () {
+	console.log("Neužstok saulės");
 });
-
--- Javascript/2025-01-27 callbacks + events/index.js
+++ Javascript/2025-01-27 callbacks + events/index.js
@@ -1,15 +1,20 @@
-// Callback funkcija - 
+// Callback funkcija -
 // tai funkcija, kuri yra paduodama kitai funkcijai kaip parametras.
 
 const h2 = document.querySelector("h2");
 
-h2.addEventListener('click', function(){
-    console.log("Ivyko nukopijavimas lietuvisko h2 elemento");
-})
+// Event listeneris - Vartotojo veiksmų stebėjimas.
+// Vartotojo veiksmai gali būti praktiškai bet kokie (paspaudimai, pelytės pajudinimai, paspaudimo pradžia, pabaiga resize, zoom, kopijavimas ir t.t.)
+// Į šiuos veiksmus, reaguojame paduodami atsišaukiančią funkciją.
+// Tarsi sakytume, kai įvyks nukopijavimas h2 elementui, atlikime tokį veiksmą.
+h2.addEventListener("copy", function () {
+	console.log("Ivyko nukopijavimas lietuvisko h2 elemento");
+});
 
-apskaiciuotiPvm(100, 18, (naujaKaina)=>{
-    // document.querySelector("h3#lt").innerText = naujaKaina;
-    console.log(naujaKaina.toUpperCase());
+// Paskutinis funkcijos parametras - atsišaukianti funkcija - callback. Atsitinkanti prieš return'ą
+apskaiciuotiPvm(100, 18, (naujaKaina) => {
+	// document.querySelector("h3#lt").innerText = naujaKaina;
+	console.log(naujaKaina.toUpperCase());
 });
 
 // function atnaujintiLietuviskaKainaPuslapyje(naujaKaina)
@@ -17,28 +22,17 @@ apskaiciuotiPvm(100, 18, (naujaKaina)=>{
 //     document.querySelector("h3#lt").innerText = naujaKaina;
 // }
 
-
-
-
-
-
-
-
-function apskaiciuotiPvm(kaina, pvm, callback)
-{
-    const galutineKaina = kaina * (100 + pvm) / 100;
-    callback(`Galutinė, apskaičiuota kaina yra ${galutineKaina}`);
-    return galutineKaina;   
-    // 21%
-    // 18%
+function apskaiciuotiPvm(kaina, pvm, callback) {
+	const galutineKaina = (kaina * (100 + pvm)) / 100;
+	callback(`Galutinė, apskaičiuota kaina yra ${galutineKaina}`);
+	return galutineKaina;
+	// 21%
+	// 18%
 }
 
-function atnaujintiVokiskaKainaPuslapyje(naujaKaina)
-{
-    document.querySelector("h3#de").innerText = naujaKaina;
+function atnaujintiVokiskaKainaPuslapyje(naujaKaina) {
+	document.querySelector("h3#de").innerText = naujaKaina;
 }
-
-
 
 const masyvas = [1, 2, 3, 4, 5, 6];
 console.log(masyvas.filter((reiksme) => reiksme % 2 !== 0));
